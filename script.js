b4d42abbb9cfee6dc5ed6be616bab2c2e9290da6
sameh <sameh080081@gmail.com>
2022-05-11 08:16:04 +0400

    update reviewer [OPTIONAL] changes.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const burger = document.getElementById('burger');
 const menuItems = document.getElementById('menu-block');
 const close = document.getElementById('close');
-const item1 = document.getElementById('menuitem1');
-const item2 = document.getElementById('menuitem2');
-const item3 = document.getElementById('menuitem3');
+const items = document.querySelectorAll('.menu-items');
 function show() {
   menuItems.style.display = 'flex';
 }
@@ -12,6 +10,4 @@ function hide() {
 }
 burger.addEventListener('click', show);
 close.addEventListener('click', hide);
-item1.addEventListener('click', hide);
-item2.addEventListener('click', hide);
-item3.addEventListener('click', hide);
+items.forEach(item => item.addEventListener('click', hide));
