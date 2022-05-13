cdb7a473638dcbd829123b2a7be840c9c48f81fe
sameh <sameh080081@gmail.com>
2022-05-13 11:26:34 +0400

    added close button.

--- popup.js
+++ popup.js
@@ -1,47 +1,47 @@
 const projects = [{
-    image:'./images/snap/1.jpg' ,
-    title:'Multi-Post Stories Gain+Glory' ,
-    tags:['rubyOnrails','css','javascript'],
+    image:'./images/snap/1.png' ,
+    title:'Multi-Post Stories Gpngain+Glory' ,
+    tags:['Ruby on rails','css','JavaScript'],
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     btnLive:'./images/icons/see-live-icon.jbg',
     btnSource:'./images/icons/Vector.jbg',
 },
 {
-    image:'./images/snap/1.jpg' ,
+    image:'./images/snap/2.png' ,
     title:'Multi-Post Stories Gain+Glory' ,
-    tags:['rubyOnrails','css','javascript'],
+    tags:['Ruby on rails','css','JavaScript'],
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     btnLive:'./images/icons/see-live-icon.jbg',
     btnSource:'./images/icons/Vector.jbg',
 },
 {
-    image:'./images/snap/1.jpg' ,
+    image:'./images/snap/3.png' ,
     title:'Multi-Post Stories Gain+Glory' ,
-    tags:['rubyOnrails','css','javascript'],
+    tags:['Ruby on rails','css','JavaScript'],
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     btnLive:'./images/icons/see-live-icon.jbg',
     btnSource:'./images/icons/Vector.jbg',
 },
 {
-    image:'./images/snap/1.jpg' ,
+    image:'./images/snap/4.png' ,
     title:'Multi-Post Stories Gain+Glory' ,
-    tags:['rubyOnrails','css','javascript'],
+    tags:['Ruby on rails','css','JavaScript'],
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     btnLive:'./images/icons/see-live-icon.jbg',
     btnSource:'./images/icons/Vector.jbg',
 },
 {
-    image:'./images/snap/1.jpg' ,
+    image:'./images/snap/1.png' ,
     title:'Multi-Post Stories Gain+Glory' ,
-    tags:['rubyOnrails','css','javascript'],
+    tags:['Ruby on rails','css','JavaScript'],
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     btnLive:'./images/icons/see-live-icon.jbg',
     btnSource:'./images/icons/Vector.jbg',
 },
 {
-    image:'./images/snap/1.jpg' ,
+    image:'./images/snap/2.png' ,
     title:'Multi-Post Stories Gain+Glory' ,
-    tags:['rubyOnrails','css','javascript'],
+    tags:['Ruby on rails','css','JavaScript'],
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     btnLive:'./images/icons/see-live-icon.jbg',
     btnSource:'./images/icons/Vector.jbg',
@@ -54,25 +54,29 @@ for(let i=0;i<projectsBtn.length;i++){
 }
 
 function seeProjects(index){
-    const works = document.querySelector('.works');
-    const container = document.querySelector('.popup-container');
-    container.style.display = 'block';
-
-    projectsBtn[index].style.background = 'black';
-    container.style.background = 'black';
-
-    container.innerHTML = `
-    <img src=${projectsBtn[index].image} alt="popup-project-img" class="project-img">
-    <h1 class="title">${projectsBtn[index].title}</h1>
+    const {image,title,tags,description,btnLive,btnSource,} = projects[index];
+    const container = document.createElement('div');
+    container.classList.add('popup-container');
+    container.innerHTML =
+    `<img src=${image}  class="popup-project-img1">
+    <img src="./images/icons/Iconx.png"  class="close-icon">
+    <h1 class="title">${title}</h1>
     <div class="tags-container">
-    <div class="tag">${projectsBtn[index].tags[0]}</div>
-    <div class="tag">${projectsBtn[index].tags[1]}</div>
-    <div class="tag">${projectsBtn[index].tags[2]}</div>
+    <div class="tag">${tags[0]}</div>
+    <div class="tag">${tags[1]}</div>
+    <div class="tag">${tags[2]}</div>
     </div>
-    <p class="description">${projectsBtn[index].description}</p>
+    <p>${description}</p>
     <div class="btn-container">
-    <button class="live-demo">See Live<img src=${projectsBtn[index].btnLive}></button>
-    <button class="see-source">See Source<img src=${projectsBtn[index].btnSource}></button>
-    </div>
-    `;
+    <button type="button" class="btnLive">See Live<img src="./images/icons/see-live-icon.png"></button>
+    <button type="button" class="btnSource">See Source<img src="./images/icons/Vector.png"></button>
+    </div>`;
+    document.body.appendChild(container);
+    container.style.display = 'block';
+    
+    const btnclose = document.querySelector('.close-icon');
+    btnclose.addEventListener('click' ,closemethod);
+    function closemethod(){
+        container.style.display = "none";
+    }
 }
