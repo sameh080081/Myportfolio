b299755847c07f660e355c76c5d351d703013bff
sameh <sameh080081@gmail.com>
2022-05-13 13:51:08 +0400

    add validation javascript file.

---
+++ js/formvalidation.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('contact-form');
+const emailInput = document.getElementById('email');
+const formButton = document.getElementById('submit');
+
+form.addEventListener('submit', (event) => {
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    formButton.setCustomValidity('Please put your email characters in lower case my g!');
+    formButton.reportValidity();
+    event.preventDefault();
+  }
+});
+
+emailInput.addEventListener('input', () => {
+  formButton.setCustomValidity('');
+});
